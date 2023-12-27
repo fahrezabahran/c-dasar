1d5161b9b3582b93471277dc866adb308411a466
Fahreza Bahran <fahreza.bahran@bni.co.id>
2023-12-27 15:09:26 +0700

    ref

---
+++ CSharpBasic/CSharpBasic/ClassRef.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    public class ClassRef
+    {
+        public void RunRef()
+        {
+            int nilai = 10;
+            Console.WriteLine("Nilai sebelum pemanggilan metode: " + nilai);
+
+            // Panggil metode dengan parameter ref
+            ModifikasiNilai(ref nilai);
+
+            // Value variable nilai berubah, padahal tidak ada assign value ke nilai ini dikarnakan ref
+            Console.WriteLine("Nilai setelah pemanggilan metode: " + nilai);
+        }
+
+        private static void ModifikasiNilai(ref int x)
+        {
+            // Modifikasi nilai variabel yang dilewatkan sebagai parameter ref
+            x = 20;
+        }
+    }
+}
---
+++ CSharpBasic/CSharpBasic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpBasic
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ClassRef classRef = new();
+            classRef.RunRef();
+        }
+
+
+    }
+}
